f8d2455c4e026398abf57031a859fffda35dec18
Yu Zhang <yuzhang@vmware.com>
2018-12-08 18:31:41 +0800

    The first iOS Class code

--- Consentration/ViewController.swift
+++ Consentration/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+    var flipCount = 0 {
+        didSet{
+            flipConutLabel.text = "Flips: \(flipCount)"
+        }
     }
 
+    var emojiArray = ["👻","🎃","😂","😞","😞","🎃","😂","👻"]
+    
+    @IBOutlet weak var flipConutLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+//        print ("Agh! A ghost!")
+        flipCount += 1
+        if let cardNumer = cardButtons.index(of: sender){
+            print("cardNumer= \(cardNumer)")
+            flipCard(withEmoji: emojiArray[cardNumer], on: sender)
+        } else {
+            print("Chosen card was not in cardButtons!")
+        }
+//        print("cardNumer= \(cardNumer)")
+        
+//        flipCard(withEmoji: emojiArray[cardNumer], on: sender)
+    }
 
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        print("flipCard withEmoji: \(emoji)")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
 }
 
